885ea5f5c66d3b66a914f7859b42bfbdca55a92b
edouardglx <edouard.gloux@zenika.com>
2023-03-27 16:28:00 +0200

    Tested Search component

--- src/app/components/search-bar/search-bar.component.spec.ts
+++ src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,23 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { screen, fireEvent, render } from '@testing-library/angular';
 import { SearchBarComponent } from './search-bar.component';
+import '@testing-library/jest-dom';
+import '@testing-library/user-event';
 
 describe('SearchBarComponent', () => {
-  let component: SearchBarComponent;
-  let fixture: ComponentFixture<SearchBarComponent>;
-
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(SearchBarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await render(SearchBarComponent, {
+      declarations: [SearchBarComponent],
+      imports: [FormsModule],
+    });
+  });
+  it('searchButton should be present', () => {
+    const buttonSearchElem = screen.getByText('Search');
+    expect(buttonSearchElem).toBeInTheDocument();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should render Clear button', async () => {
+    const inputElem = screen.getByPlaceholderText('Search') as HTMLInputElement;
+    fireEvent.input(inputElem, { target: { value: 'Hello' } });
+    const clearButtonElement = await screen.findByText('Clear');
+    expect(clearButtonElement).toBeInTheDocument();
+  });
+
+  it('Should not render Clear button', async () => {
+    const inputElem = screen.getByPlaceholderText('Search') as HTMLInputElement;
+    await fireEvent.input(inputElem, { target: { value: '' } });
+    const clearButtonElement = screen.queryByText('Clear');
+    expect(clearButtonElement).not.toBeInTheDocument();
+  });
+
+  it('The input should be empty', async () => {
+    const inputElem = screen.getByPlaceholderText('Search') as HTMLInputElement;
+    fireEvent.input(inputElem, { target: { value: 'test' } });
+    const clearButtonElement = await screen.findByText('Clear');
+    fireEvent.click(clearButtonElement);
+    setTimeout(() => {
+      expect(inputElem.value).toBe('');
+    }, 500);
   });
 });
